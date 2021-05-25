8cbb84869b4568c318b9c0921db1518942fc5c72
Shivani Mehta <shivanimehta7n7@gmail.com>
2021-05-26 02:19:02 +0530

    changed API link

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -30,7 +30,7 @@ export default function TextEditor() {
   const [quill, setQuill] = useState();
 
   useEffect(() => {
-    const s = io("http://localhost:3001");
+    const s = io("https://annotepad.herokuapp.com/");
     setSocket(s);
 
     return () => {
